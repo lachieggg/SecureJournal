d19de4cc7e1d98a7b448931ef0e7a158ea126eff
Lachlan Grant <4781893+lachieggg@users.noreply.github.com>
2020-10-18 12:22:30 +1100

    Update shared.py

--- lib/shared.py
+++ lib/shared.py
@@ -3,6 +3,8 @@ from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.backends import default_backend
 
+import cryptography
+
 # Libraries used by both scripts
 from cryptography.fernet import Fernet
 
